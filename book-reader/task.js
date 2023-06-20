12f457e5b039d5815011fd8e5a58903d359e43ea
Yury Vereskun <satriks@mail.ru>
2023-06-20 15:33:42 +0300

    book-reader done

--- book-reader/task.js
+++ book-reader/task.js
@@ -1,9 +1,46 @@
-
+(function () {
+    
 let sizes = document.querySelectorAll('.font-size')
+let colors = document.querySelectorAll('.color')
+colors.forEach( el => el.addEventListener('click', changeColor))
+
 sizes.forEach((el) => el.addEventListener('click', chengekSize ))
-const textStr = document.querySelector('.book__content')
+const textStr = document.querySelector('.book')
 
+function changeColor(event) {
 
+    event.preventDefault()
+    
+    if (this.dataset.textColor) {
+        textColor(this.dataset.textColor)
+        this.parentElement.querySelectorAll('.color').forEach(el => el.classList.remove('color_active'))
+        this.classList.add('color_active')
+    } else if (this.dataset.bgColor) {
+        bgColor(this.dataset.bgColor)
+        this.parentElement.querySelectorAll('.color').forEach(el => el.classList.remove('color_active'))
+        this.classList.add('color_active')
+    }
+
+    function textColor(color) {
+        
+        for (let i of textStr.classList) {
+            if (i.startsWith('book_color')) {
+                textStr.classList.remove(i)
+            }
+        }
+        textStr.classList.add(`book_color-${color}`)
+    }
+
+    function bgColor(color) {
+        for (let i of textStr.classList) {
+            if (i.startsWith('book_bg')) {
+                textStr.classList.remove(i)
+            }
+        }
+
+        textStr.classList.add(`book_bg-${color}`)
+    } 
+}
 
 function chengekSize(event) {
     event.preventDefault()
@@ -13,7 +50,7 @@ function chengekSize(event) {
     if (Array.from(el.classList).includes('font-size_active')) {
         el.classList.remove('font-size_active')
     }})
-    
+
     switch (this.dataset.size) {
         case 'big':
             textStr.classList.add('book_fs-big')
@@ -34,3 +71,5 @@ function chengekSize(event) {
             break;
     }
 }
+
+})()
