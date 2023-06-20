022e29e84cf8a51453984fe8dbee7a2ef33ddb8e
Yury Vereskun <satriks@mail.ru>
2023-06-20 14:21:59 +0300

    ads done

--- ads/task.js
+++ ads/task.js
@@ -1,16 +1,29 @@
 
+(function () {
+    
+    let meInter = setInterval ( changeAdd , 1000)
+
+    function changeAdd() {
+        const adds = Array.from(document.querySelector('.rotator').querySelectorAll('.rotator__case'))
+        
+        adds.forEach( el => {
+            el.classList.remove('rotator__case_active')
+        })
+
+        adds[Math.floor(Math.random() * adds.length)].classList.add('rotator__case_active')
 
 
-function changeAdd() {
-    console.log('tik');
-   const adds = Array.from(document.querySelectorAll('.rotator__case'))
+        for (let i of adds) {
+            [s1, s2] = i.classList
+            if (s2) {
+                
+                window.clearInterval(meInter)
+                let {speed, color} = i.dataset
+                i.style.color = color
+                meInter = setInterval ( changeAdd , speed)
+            }
+        }
+    }
 
-    adds.forEach( el => {
-        el.classList.remove('rotator__case_active')
-    })
+})()
 
-    adds[Math.floor(Math.random() * adds.length)].classList.add('rotator__case_active')
- 
-}
-
-setInterval ( changeAdd , 1000)
--- reveal/task.js
+++ reveal/task.js
@@ -2,7 +2,6 @@
 window.addEventListener('scroll', reveals)
 
 function reveals() {
-    console.log('reveals');
     const reveals = document.querySelectorAll('.reveal').forEach( el => task(el))  
 
     function  task(reveal) {
@@ -10,8 +9,6 @@ function reveals() {
 
         if (bottom < window.innerHeight && top > 0) {
             reveal.classList.add('reveal_active')
-            console.log('ok bot');
-            console.log( reveal.classList);
         } else (reveal.classList.remove('reveal_active'))
 
     }
